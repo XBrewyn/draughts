1fe5b643faa56dbaf405f014f54c4f21ca8459fb
Brewyn <Brewyn@outlook.com>
2022-01-29 01:08:24 -0400

    fix test (1/29/2022)

--- tests/app/Piece/index.test.ts
+++ tests/app/Piece/index.test.ts
@@ -1,30 +1,76 @@
+import Board from '../../../app/Board'
 import Piece from '../../../app/Piece'
-import { Color } from '../../../app/Tools/enums'
+import BlackPiece from '../../../app/Piece/BlackPiece'
+import { EatBlackPiece } from '../../../app/Piece/Eat'
+import { MoveWhitePiece } from '../../../app/Piece/Move'
+import WhitePiece from '../../../app/Piece/WhitePiece'
+import { Color, Icon } from '../../../app/Tools/enums'
 
-class TestPiece extends Piece {}
+class TestPiece extends Piece {
+  constructor (position: string) {
+    super(Color.WHITE, position)
 
-const position: string = 'testPos'
-const pieceBlack: TestPiece = new TestPiece(Color.BLACK, position)
-const pieceWhite: TestPiece = new TestPiece(Color.WHITE, position)
+    this._typeMove = new MoveWhitePiece()
+    this._typeEat = new EatBlackPiece()
+  }
+}
+
+const enemyPosition: string = 'c6'
+const initialPosition: string = 'a4'
+
+let piece: TestPiece
+let board: Board
 
 describe('Piece', () => {
-  describe('White', () => {
-    test('should be a color white', () => {
-      expect(pieceWhite.color).toBe(Color.WHITE)
-    })
+  beforeEach(() => {
+    board = new Board()
+    piece = new TestPiece(initialPosition)
 
-    test('should have a position', () => {
-      expect(pieceBlack.position).toBe(position)
+    board.addPiece({
+      WhitePiece,
+      BlackPiece
     })
   })
 
-  describe('Black', () => {
-    test('should be a color black', () => {
-      expect(pieceBlack.color).toBe(Color.BLACK)
-    })
+  it('default properties', () => {
+    expect(piece.color).toEqual(Color.WHITE)
+    expect(piece.position).toEqual(initialPosition)
+    expect(piece.isKing).toEqual(false)
+    expect(piece.icon).toEqual(Icon.WHITE)
+  })
 
-    test('should have a position', () => {
-      expect(pieceBlack.position).toBe(position)
-    })
+  it('should be a king piece', () => {
+    const isKing: boolean = true
+
+    piece.isKing = isKing
+
+    expect(piece.isKing).toEqual(isKing)
+  })
+
+  it('should called the method canEat()', () => {
+    const position: string = 'd7'
+    const enemyPiece: BlackPiece = new BlackPiece(position)
+
+    board.update(enemyPiece, enemyPosition)
+    board.update(piece, 'b5')
+
+    expect(piece.canEat(board, position)).toEqual(true)
+  })
+
+  it('should called the method canMove()', () => {
+    const canMove: boolean = piece.canMove(board, 'b5')
+
+    expect(canMove).toEqual(true)
+  })
+
+  it('should returns the targetPos', () => {
+    const position: string = 'd7'
+    const enemyPiece: BlackPiece = new BlackPiece(position)
+
+    board.update(enemyPiece, enemyPosition)
+    board.update(piece, 'b5')
+    piece.canEat(board, position)
+
+    expect(piece.targetPos).toEqual(enemyPosition)
   })
 })
